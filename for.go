2dd81102954f43fd81d5523aad7562c7868186c4
Diego Cassandri <diego.cassandri@gmail.com>
2022-11-03 11:29:14 -0300

    exercices

---
+++ for.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//for is Go’s only looping construct. Here are some basic types of for loops.
+
+	//The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	//A classic initial/condition/after for loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	//for without a condition will loop repeatedly until you break out of the loop or return from the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	//You can also continue to the next iteration of the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+}
